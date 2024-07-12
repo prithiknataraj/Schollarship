179997be3ebe20203ae69c58ee206bc75aae9075
Prith <93419588+prithiknataraj@users.noreply.github.com>
2024-07-12 10:02:44 +0530

    updation

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def Scholarship():
-    return render_template("scholarship.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
